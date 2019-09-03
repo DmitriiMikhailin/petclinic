aecc69737584d78e74e8d87b47cec739cc2a841b
Dmitrii Mikhailin <mikhaylin.d@gmail.com>
2019-09-03 12:55:08 +0300

    implement interfaces for services. remove maven-release-plugin. closes #5

---
+++ pet-clinic-data/src/main/java/services/VetService.java
@@ -0,0 +1,14 @@
+package services;
+
+import ru.mikhailin.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long Id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
