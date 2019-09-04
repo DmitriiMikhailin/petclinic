c76b18d9bb7d6f890dc73eaf78e33266f79238ea
Dmitrii Mikhailin <mikhaylin.d@gmail.com>
2019-09-04 11:58:54 +0300

    Added index page and controller. closes #8

---
+++ pet-clinic-web/src/main/java/ru/mikhailin/petclinic/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package ru.mikhailin.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"", "/", "index"})
+    public String getIndex() {
+        return "index";
+    }
+}
