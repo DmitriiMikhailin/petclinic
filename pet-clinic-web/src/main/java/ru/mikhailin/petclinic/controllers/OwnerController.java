40d7ed747ff28cd1da4b89ed8628b16bb3b314f6
Dmitrii Mikhailin <mikhaylin.d@gmail.com>
2019-09-10 13:04:04 +0300

    Listed all owners on owner index page. closes #16

--- pet-clinic-web/src/main/java/ru/mikhailin/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ru/mikhailin/petclinic/controllers/OwnerController.java
@@ -1,13 +1,22 @@
 package ru.mikhailin.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.mikhailin.petclinic.services.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/index"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
