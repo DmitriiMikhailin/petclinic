7067c6f4581672e5d8c6916ae6daafab478694ed
Dmitrii Mikhailin <mikhaylin.d@gmail.com>
2019-09-10 13:19:47 +0300

    Listed all vets on vet index page. closes #15

--- pet-clinic-web/src/main/java/ru/mikhailin/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ru/mikhailin/petclinic/controllers/VetController.java
@@ -1,12 +1,21 @@
 package ru.mikhailin.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.mikhailin.petclinic.services.VetService;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"vets", "vets/index"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
